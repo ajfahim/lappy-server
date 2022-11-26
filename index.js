68cb5c41fd23943e2f3ab192fb9f7e87c3c49ab3
ajfahim <ajfahim52@gmail.com>
2022-11-26 18:26:06 +0600

    users post method added

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json())
 
-const uri = "mongodb+srv://<username>:<password>@cluster0.uh9rxet.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.uh9rxet.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 //routes
@@ -22,6 +22,31 @@ app.get('/', (req, res) => {
 })
 
 
+
+async function run() {
+    try {
+        const productsCollection = client.db('lappy').collection('products');
+        const usersCollection = client.db('lappy').collection('users');
+
+        app.get("/advertised", async (req, res) => {
+            const query = {
+                isAdvertised: true
+            }
+            const result = await productsCollection.find(query).toArray();
+            res.send(result)
+        });
+
+        app.post("/users", async (req, res) => {
+            const user = req.body;
+            const result = usersCollection.insertOne(user);
+            res.send(result)
+        })
+    }
+    finally {
+
+    }
+}
+run().catch(console.log);
 
 
 
