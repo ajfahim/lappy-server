33a07ead00a0327d7d7f43d894027585cc8bbeab
ajfahim <ajfahim52@gmail.com>
2022-11-28 08:45:58 +0600

    endpoint to check if user is seller has been added

--- index.js
+++ index.js
@@ -42,6 +42,20 @@ async function run() {
             res.send(result)
         })
 
+        app.get('/users/admin/:email', async (req, res) => {
+            const email = req.params.email;
+            const query = { email }
+            const user = await usersCollection.findOne(query);
+            res.send({ isAdmin: user?.role === 'admin' });
+        })
+
+        app.get('/users/seller/:email', async (req, res) => {
+            const email = req.params.email;
+            const query = { email }
+            const user = await usersCollection.findOne(query);
+            res.send({ isSeller: user?.role === 'seller' });
+        })
+
         app.get('/jwt', async (req, res) => {
             const email = req.query.email;
             const query = { email: email };
